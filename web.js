e7fee74c6d6d1c8453e72b4483c5855eed417b06
Greg Little <glittle@gmail.com>
2013-02-06 06:44:23 -0800

    test

--- web.js
+++ web.js
@@ -1,13 +1,58 @@
 
-var express = require('express');
+require('./u.js')
 
-var app = express.createServer(express.logger());
+var db = require('mongojs').connect(process.env.MONGOHQ_URL || "test")
+var app = require('express').createServer()
+
+
+
+// var odesk = require('node-odesk')
+// var o = new odesk(process.env.ODESK_API_KEY, process.env.ODESK_API_SECRET)
+
+
+var passport = require('passport')
+var OAuthStrategy = require('passport-oauth').OAuthStrategy;
+
+passport.use('oDesk', new OAuthStrategy({
+    requestTokenURL: 'https://www.odesk.com/api/auth/v1/oauth/token/request',
+    accessTokenURL: 'https://www.odesk.com/api/auth/v1/oauth/token/access',
+    userAuthorizationURL: 'https://www.odesk.com/services/api/auth',
+    consumerKey: process.env.ODESK_API_KEY,
+    consumerSecret: process.env.ODESK_API_SECRET,
+    callbackURL: 'https://sheltered-hamlet-7258.herokuapp.com/odesk-login-callback'
+  },
+  function(token, tokenSecret, profile, done) {
+  	done(null, { "name" : "hi" })
+    // User.findOrCreate(..., function(err, user) {
+    //   done(err, user);
+    // });
+  }
+));
+
+
+app.all('/login',
+  passport.authenticate('oDesk', { successRedirect: '/',
+                                   failureRedirect: '/login' }));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+	// db.collection("test").find(function (err, data) {
+	// 	console.log("hi: " + _.json(data))
+	// })
+	console.log("hi?")
+  	response.send('Hello World!')
+})
 
-var port = process.env.PORT || 5000;
+// app.get('/login', function (req, res) {
+// 	o.OAuth.getAuthorizeUrl('https://sheltered-hamlet-7258.herokuapp.com/odesk-login-callback', function(error, url, requestToken, requestTokenSecret) {
+// 		res.redirect(url)
+// 	})
+// })
+
+// app.get('/odesk-login-callback', function (req, res) {
+// 	res.send("got here!")
+// })
+
+var port = process.env.PORT || 5000
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+	console.log("Listening on " + port)
+})

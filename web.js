bbeed0634907608eb33c3d6ddb34ec9217414c23
Greg Little <glittle@gmail.com>
2013-02-07 02:42:45 -0800

    good state : oDesk login working

--- web.js
+++ web.js
@@ -1,21 +1,10 @@
 
 require('./u.js')
 
-var db = require('mongojs').connect(process.env.MONGOHQ_URL || "test")
-var express = require('express')
-var app = express.createServer()
-
-
-
-// var odesk = require('node-odesk')
-// var o = new odesk(process.env.ODESK_API_KEY, process.env.ODESK_API_SECRET)
-
+var db = require('mongojs').connect(process.env.MONGOHQ_URL)
 
 var passport = require('passport')
 var OAuthStrategy = require('passport-oauth').OAuthStrategy;
-
-app.use(express.cookieParser())
-app.use(express.session({ secret : process.env.SESSION_SECRET || "nosecret" }))
 
 passport.use('oDesk', new OAuthStrategy({
     requestTokenURL: 'https://www.odesk.com/api/auth/v1/oauth/token/request',
@@ -23,52 +12,58 @@ passport.use('oDesk', new OAuthStrategy({
     userAuthorizationURL: 'https://www.odesk.com/services/api/auth',
     consumerKey: process.env.ODESK_API_KEY,
     consumerSecret: process.env.ODESK_API_SECRET,
-    callbackURL: 'https://sheltered-hamlet-7258.herokuapp.com/odesk-login-callback'
-  },
-  function(token, tokenSecret, profile, done) {
-  	done(null, {
-  		token : token,
-  		tokenSecret : tokenSecret,
-  		profile : profile,
-  		name : "hi"
-  	})
-    // User.findOrCreate(..., function(err, user) {
-    //   done(err, user);
-    // });
-  }
-));
+    callbackURL: process.env.HOST + '/login-callback'
+}, function(token, tokenSecret, profile, done) {
+	var odesk = require('node-odesk')
+	var o = new odesk(process.env.ODESK_API_KEY, process.env.ODESK_API_SECRET)
+	o.OAuth.accessToken = token;
+	o.OAuth.accessTokenSecret = tokenSecret;  	
+	o.get('auth/v1/info', function(error, data) {
+		if (error) return done(error)
+	    var user = {
+	    	_id : data.auth_user.uid,
+	    	name : data.auth_user.first_name + " " + data.auth_user.last_name,
+	    	img : data.info.portrait_100_img,
+	    	country : data.info.location.country,
+	    	profile : data.info.profile_url
+	    }
+	    db.collection('user').update({ _id : user._id }, user, { upsert : true }, function (err) {
+	    	if (err) return done(error)
+    		done(null, user)
+	    })
+	})
+}))
 
-
-app.all('/login',
-  passport.authenticate('oDesk', { successRedirect: '/',
-                                   failureRedirect: '/login' }));
-
-app.get('/odesk-login-callback', function(req, res) {
-	// db.collection("test").find(function (err, data) {
-	// 	console.log("hi: " + _.json(data))
-	// })
-	console.log("hi?: " + _.json(req.user))
-  	res.send('Hello World!')
+passport.serializeUser(function (user, done) {
+	done(null, user._id)
 })
+
+passport.deserializeUser(function (id, done) {
+	db.collection('user').findOne({ _id : id }, function (err, data) {
+		if (err) return done(err)
+		done(null, data)
+	})
+})
+
+var express = require('express')
+var app = express.createServer()
+
+app.use(express.cookieParser())
+app.use(express.bodyParser())
+app.use(express.session({ secret : process.env.SESSION_SECRET }))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/login', passport.authenticate('oDesk'))
+app.get('/login-callback', passport.authenticate('oDesk', {
+	successRedirect: '/',
+	failureRedirect: '/login'
+}))
+
 app.get('/', function(req, res) {
-	// db.collection("test").find(function (err, data) {
-	// 	console.log("hi: " + _.json(data))
-	// })
-	console.log("hi?: " + _.json(req.user))
-  	res.send('Hello World!')
+  	res.send('Hello: ' + _.escapeXml("" + _.json(req.user, true)))
 })
 
-// app.get('/login', function (req, res) {
-// 	o.OAuth.getAuthorizeUrl('https://sheltered-hamlet-7258.herokuapp.com/odesk-login-callback', function(error, url, requestToken, requestTokenSecret) {
-// 		res.redirect(url)
-// 	})
-// })
-
-// app.get('/odesk-login-callback', function (req, res) {
-// 	res.send("got here!")
-// })
-
-var port = process.env.PORT || 5000
-app.listen(port, function() {
-	console.log("Listening on " + port)
+app.listen(process.env.PORT, function() {
+	console.log("go to " + process.env.HOST)
 })
